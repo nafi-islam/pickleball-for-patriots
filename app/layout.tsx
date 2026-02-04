681e6ede2d2dc3d0162294f23fa57b07b3df3dd5
nafi-islam <nafi-islam@users.noreply.github.com>
2026-02-04 15:15:44 -0600

    fix fouc might need to adjust clerk provider later

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Geist, Geist_Mono } from "next/font/google";
 import "antd/dist/reset.css";
 import "./globals.css";
@@ -25,14 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AntdRegistry>
+          <ClerkProvider>{children}</ClerkProvider>
+        </AntdRegistry>
+      </body>
+    </html>
   );
 }
